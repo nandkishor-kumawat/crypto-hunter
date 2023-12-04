1f20a272eeebcedd36ee4d86076223f11271f734
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-12-05 01:30:58 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -48,7 +48,7 @@ export const getHistoricalChart = async (id: string, days = 365, currency: strin
 
 export const getTrendingCoins = async (currency: string) => {
     const url = `https://coingecko.p.rapidapi.com/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
-    const url2 = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
+    // const url2 = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
     const options = {
         method: 'GET',
         headers: {
